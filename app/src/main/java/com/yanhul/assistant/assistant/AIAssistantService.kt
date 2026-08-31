489ee0f09ac9e4a8a59103afa1f73fbf87d57576
yanhul <116991963+yanhul@users.noreply.github.com>
2026-08-31 21:42:28 +0700

    feat: implement voice interaction service

---
+++ app/src/main/java/com/yanhul/assistant/assistant/AIAssistantService.kt
@@ -0,0 +1,9 @@
+package com.yanhul.assistant.assistant
+
+import android.service.voice.VoiceInteractionService
+
+class AIAssistantService : VoiceInteractionService() {
+    override fun onReady() {
+        super.onReady()
+    }
+}
