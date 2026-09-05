98c31e6c51722eb8c2107034465d8b7230467d8d
yanhul <116991963+yanhul@users.noreply.github.com>
2026-09-05 22:44:05 +0700

    test: add temporary E2E CI feedback probe

---
+++ app/src/test/java/E2ECiFeedbackProbeTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class E2ECiFeedbackProbeTest {
+    @Test
+    public void temporaryE2EProbe() {
+        throw new AssertionError("temporary E2E CI feedback probe");
+    }
+}
